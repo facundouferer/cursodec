2e5288c8423b01d48c943b1d7a83260a3bc5057c
Facundo Uferer <juanfacundouf@gmail.com>
2025-04-11 16:56:42 -0300

    arreglos

---
+++ Desafios/06MultiplicadorDeMatricesInicio.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int A[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    int B[3][3] = {
+        {9, 8, 7},
+        {6, 5, 4},
+        {3, 2, 1}
+    };
+
+    int C[3][3];
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            C[i][j] = 0;
+            for (int k = 0; k < 3; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+    
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d\t ", C[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
