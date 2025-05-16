813cd8d4f00a6b69d2f1a42150d18c02d407863a
Facundo Uferer <juanfacundouf@gmail.com>
2025-05-16 18:59:34 -0300

    fix

--- Problemas/10Estructuras/02ListaDeProductos.c
+++ Problemas/10Estructuras/02ListaDeProductos.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
-
+#define TAM_NOMBRE 50
+#define MAX 2
 struct Producto{
-    char nombre[50];
+    char nombre[TAM_NOMBRE];
     float precio;
     int cantidad;
 } ;
 
 int main() {
-    struct Producto productos[3];
+    struct Producto productos[MAX];
     int i;
 
-    for (i = 0; i < 3; i++) {
+    for (i = 0; i < MAX; i++) {
         printf("Producto %d:\n", i + 1);
         printf("Nombre: ");
         scanf(" %[^\n]", productos[i].nombre);
@@ -21,10 +22,10 @@ int main() {
         printf("\n");
     }
 
-    printf("PRODUCTO \tCANTIDAD \t\tPRECIO \tTOTAL\n");
-    for (i = 0; i < 3; i++) {
+    printf("PRODUCTO \tCANTIDAD \tPRECIO \tTOTAL\n");
+    for (i = 0; i < MAX; i++) {
         float valor_total = productos[i].precio * productos[i].cantidad;
-        printf("%s \t%d \t\t%.2f \t$%.2f\n", productos[i].nombre, productos[i].cantidad, productos[i].precio, valor_total);
+        printf("%s \t%d \t%.2f \t$%.2f\n", productos[i].nombre, productos[i].cantidad, productos[i].precio, valor_total);
     }
 
     return 0;
